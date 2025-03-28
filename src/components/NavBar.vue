ad55cfec99126c80229bdbdc7f0ca8f4984d33fd
edson.sarmiento <eaargueta@innovacion.gob.sv>
2025-03-28 08:31:28 -0600

    feat: instalacion de nuevas dependencias y navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,69 @@
+
+<template>
+  <div class="card">
+    <Menubar :model="items">
+      <template #end>
+        <div class="flex items-center gap-2">
+          <Button label="Iniciar sesión" severity="info" variant="text" raised />
+        </div>
+      </template>
+    </Menubar>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Menubar from 'primevue/menubar';
+import Button from 'primevue/button';
+
+const items = ref([
+  {
+    label: 'Inicio',
+    icon: 'pi pi-home'
+  },
+  {
+    label: 'Favoritos',
+    icon: 'pi pi-star'
+  },
+  {
+    label: 'Categorias',
+    icon: 'pi pi-bars',
+    items: [
+      {
+        label: 'Components',
+        icon: 'pi pi-bolt'
+      },
+      {
+        label: 'Blocks',
+        icon: 'pi pi-server'
+      },
+      {
+        label: 'UI Kit',
+        icon: 'pi pi-pencil'
+      },
+      {
+        label: 'Templates',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Apollo',
+            icon: 'pi pi-palette'
+          },
+          {
+            label: 'Ultima',
+            icon: 'pi pi-palette'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Mi carrito',
+    icon: 'pi pi-shopping-cart'
+  },
+  {
+    label: 'Mi cuenta',
+    icon: 'pi pi-user'
+  }
+]);
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+//app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: false,
+            cssLayer: false
+        }
+    }
+});
+app.mount('#app');
